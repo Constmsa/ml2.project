d0d81e3b0fa707d1ae43d3d67431d5f89e7f8051
Constmsa <constanca.marques.sa@gmail.com>
2025-05-30 14:29:37 +0100

    com hierarchical e kmeans

--- clustering_functions.py
+++ clustering_functions.py
@@ -1,9 +1,10 @@
-import umap
+
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from minisom import MiniSom
 import numpy as np
+from pre_processing_functions import preprocess
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
 
 def som_cluster(df, features, som_size=3, iterations=5000, sigma=1.0, learning_rate=0.5):
    
@@ -27,4 +28,39 @@ def som_cluster(df, features, som_size=3, iterations=5000, sigma=1.0, learning_r
     df['som_cluster'] = ([som.winner(X[i]) for i in range(0, len(X))])
     return df
 
+def kmeans_clustering(path, n_clusters = 9, random_state = 42):
+    data = preprocess(path)  
+    data= data[['lifetime_spend_groceries', 'lifetime_spend_electronics',
+        'typical_hour', 'lifetime_spend_vegetables',
+        'lifetime_spend_nonalcohol_drinks', 'lifetime_spend_alcohol_drinks',
+        'lifetime_spend_meat', 'lifetime_spend_fish', 'lifetime_spend_hygiene',
+        'lifetime_spend_videogames', 'lifetime_spend_petfood',
+        'lifetime_total_distinct_products']]
+    
+    kmeans = KMeans(n_clusters=n_clusters, random_state=random_state)
+    cluster_labels = kmeans.fit_predict(data)
 
+    # Add cluster column to the DataFrame
+    data_with_clusters = data.copy()
+    data_with_clusters['Kmeans_cluster'] = cluster_labels
+
+    return data_with_clusters
+
+def hierarchical_clustering(path, n_clusters= 9, linkage= 'ward') :
+   
+    data = preprocess(path) 
+    data= data[['lifetime_spend_groceries', 'lifetime_spend_electronics',
+        'typical_hour', 'lifetime_spend_vegetables',
+        'lifetime_spend_nonalcohol_drinks', 'lifetime_spend_alcohol_drinks',
+        'lifetime_spend_meat', 'lifetime_spend_fish', 'lifetime_spend_hygiene',
+        'lifetime_spend_videogames', 'lifetime_spend_petfood',
+        'lifetime_total_distinct_products']]
+    
+    model = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage)
+    cluster_labels = model.fit_predict(data)
+
+    # Add the cluster column to the DataFrame
+    data_with_clusters = data.copy()
+    data_with_clusters['hierarchical_cluster'] = cluster_labels
+
+    return data_with_clusters

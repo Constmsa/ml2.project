1f95d6f711ee656066748ffe5916bd745119eb7e
matildepanarra <matilde.panarra@hotmail.com>
2025-05-30 14:06:47 +0100

    umap som

--- clustering_functions.py
+++ clustering_functions.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def UMAP(df, features, target):
+def umap_soms(df, features, target):
     #Fit UMAP
     umap_object = umap.UMAP(n_neighbors=5, random_state=2)
     umap_embedding = umap_object.fit_transform(df[features])
@@ -16,9 +16,9 @@ def UMAP(df, features, target):
     plt.figure(figsize=(10, 7))
     sns.scatterplot(x='umap_1', y='umap_2', data=df, hue=df[target].astype(str),
                     palette='tab20', s=60, alpha=0.8)
-    plt.title("UMAP Projection Colored by Clusters")
-    plt.xlabel("UMAP-1")
-    plt.ylabel("UMAP-2")
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', title='Cluster')
+    plt.title("UMAP Clusters - SOM Clustering")
+    plt.xlabel("UMAP 1")
+    plt.ylabel("UMAP 2")
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', title='SOM Cluster')
     plt.tight_layout()
     plt.show()
